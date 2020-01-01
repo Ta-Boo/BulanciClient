402fdf0827eac1cb18c2997acdc6dcb2206afe73
Tobo <tobias.hladek1@gmail.com>
2020-01-01 15:52:31 +0100

    fix

---
+++ Comunication/Message/MessageFactory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tobo on 1/1/20.
+//
+
+#include "MessageFactory.h"
---
+++ Comunication/Message/MessageFactory.h
@@ -0,0 +1,15 @@
+//
+// Created by tobo on 1/1/20.
+//
+
+#ifndef BULANCICLIENT_MESSAGEFACTORY_H
+#define BULANCICLIENT_MESSAGEFACTORY_H
+
+
+class MessageFactory {
+
+
+};
+
+
+#endif //BULANCICLIENT_MESSAGEFACTORY_H
